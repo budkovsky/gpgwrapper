f9bddaa477a83e0698f8d449e7057a7f9c1a9dac
piotrek <piotrek@debian>
2019-03-24 22:33:35 +0100

    budkovsky/aid dependency for using Aid\Collection class

--- src/SignatureInfoCollection.php
+++ src/SignatureInfoCollection.php
@@ -6,51 +6,13 @@
 
 namespace Budkovsky\GpgWrapper;
 
+use Budkovsky\Aid\Collection;
+
 /**
  * Collection of SignatureInfo objects
  */
-class SignatureInfoCollection extends \ArrayObject
+class SignatureInfoCollection extends Collection
 {
-    public function __construct(array $input = [], int $flags = 0 , string $iterator_class = "ArrayIterator")
-    {
-        
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function append($value)
-    {
-        $this->offsetSet(null, $value);
-    }
-    
-    /**
-     * {@inheritDoc}
-     * @throws GpgException
-     */
-    public function offsetSet($index, $newval)
-    {
-        if ($index !== null || !$newval  instanceof SignatureInfo) {
-            throw new GpgException(sprintf(
-                'Invalid parameters for method add element collection',
-                __METHOD__,
-                SignatureInfo::class
-            ));
-        }
-        
-        parent::offsetSet(null, $newval);
-    }
-    
-    public function setFromArray(array $input): self
-    {
-        foreach ($input as $item) {
-            $this->append($this->createItem($item));
-        }
-    }
-    
-    protected function createItem(array $signatureInfo): SignatureInfo
-    {
-        return new SignatureInfo($signatureInfo);
-    }
+    protected $itemType = SignatureInfo::class;   
 }
 
